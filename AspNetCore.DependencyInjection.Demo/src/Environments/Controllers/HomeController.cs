a784160d22a80bc1c57ff00a3471f299cebb2d89
Ivan Zaruba <ivan.zaruba@nixsolutions.com>
2016-07-13 12:27:54 +0300

    Added Environments demo

---
+++ AspNetCore.DependencyInjection.Demo/src/Environments/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Services;
+
+namespace Environments.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index([FromServices] IService service)
+        {
+            return this.View(model: service.GetType().Name);
+        }
+    }
+}
---
+++ AspNetCore.DependencyInjection.Demo/src/Environments/Startup.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Services;
+
+namespace Environments
+{
+    public class Startup
+    {
+        private readonly IHostingEnvironment env;
+
+        public Startup(IHostingEnvironment env)
+        {
+            this.env = env;
+        }
+
+        /// Change ASPNETCORE_ENVIRONMENT value in <see cref="~/Properties/launchSettings.json" /> to Development, Staging or Production
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+
+            
+            if (this.env.IsDevelopment())
+            {
+                services.AddScoped<IService, LocalhostService>();
+            }
+            else
+            {
+                services.AddScoped<IService, CloudService>();
+            }
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseMvcWithDefaultRoute();
+        }
+    }
+}

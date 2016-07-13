de359f15370f6d9411d1cd83702633336928feaf
Ivan Zaruba <ivan.zaruba@nixsolutions.com>
2016-07-13 12:28:08 +0300

    Added Configuration demo

---
+++ AspNetCore.DependencyInjection.Demo/src/Configuration/Startup.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Services;
+
+namespace Configuration
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                //.AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true) // uncomment this line to see how Environment configuration affects appsettings
+                .AddEnvironmentVariables();
+
+            this.Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+
+            services.AddSingleton(this.Configuration);
+
+            var appMode = this.Configuration.GetSection("ApplicationMode")?.Value;
+
+            if (appMode == "Localhost")
+            {
+                services.AddScoped<IService, LocalhostService>();
+            }
+            else if (appMode == "Cloud")
+            {
+                services.AddScoped<IService, CloudService>();
+            }
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseMvcWithDefaultRoute();
+        }
+    }
+}

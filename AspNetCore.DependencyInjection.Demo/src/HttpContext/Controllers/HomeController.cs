0531060a849785764005cc23f9807d2c69543977
Ivan Zaruba <ivan.zaruba@nixsolutions.com>
2016-07-13 12:28:32 +0300

    Added Resolving within HTTP context demo

---
+++ AspNetCore.DependencyInjection.Demo/src/HttpContext/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using HttpContext.Extensions;
+using Microsoft.AspNetCore.Mvc;
+using Services;
+
+namespace HttpContext.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index([FromServices] IService service, bool? useLocal)
+        {
+            if (this.Request.IsAjaxRequest())
+            {
+                return this.Json(service.GetType().Name);
+            }
+
+            return this.View(model: service.GetType().Name);
+        }
+    }
+}
---
+++ AspNetCore.DependencyInjection.Demo/src/HttpContext/Startup.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Services;
+
+namespace HttpContext
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+
+            services.AddScoped<IHttpContextAccessor, HttpContextAccessor>();
+            services.AddScoped<LocalhostService>();
+            services.AddScoped<CloudService>();
+
+            services.AddScoped(serviceProvider => {
+
+                var httpContext = serviceProvider.GetRequiredService<IHttpContextAccessor>().HttpContext;
+
+                if (httpContext == null)
+                {
+                    // Разрешение сервиса происходит не в рамках HTTP запроса
+                    return null;
+                }
+
+                //// Можно использовать любые данные запроса
+
+                //var queryString = httpContext.Request.Query;
+
+                //return queryString.ContainsKey("useLocal")
+                //    ? serviceProvider.GetService<LocalhostService>() as IService
+                //    : serviceProvider.GetService<CloudService>() as IService;
+
+                //if (httpContext.Request.ContentType == "text/html")
+                //{
+                //    var formValues = httpContext.Request.Form;
+                //}
+
+                var requestHeaders = httpContext.Request.Headers;
+
+                return requestHeaders.ContainsKey("Use-local")
+                    ? serviceProvider.GetService<LocalhostService>() as IService
+                    : serviceProvider.GetService<CloudService>() as IService;
+            });
+
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
+        }
+    }
+}
